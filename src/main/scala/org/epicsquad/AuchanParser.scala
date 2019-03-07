ad943bab7c7522f9e020308edae2c4aa84217288
Denis Loshkarev <DLoshkarev@energocomm.ru>
2019-03-08 00:22:00 +0300

    change brand type

--- src/main/scala/org/epicsquad/AuchanParser.scala
+++ src/main/scala/org/epicsquad/AuchanParser.scala
@@ -68,7 +68,7 @@ class AuchanParser extends ProductParser {
         val doc = browser.get(url)
         val name = doc >> text("h1")
         val category = (doc >> elementList(".breadcrumbs__list a")).drop(1).map(_.text).mkString("/")
-        val brand = doc >> text(".prcard__feat-item strong")
+        val brand = doc >?> text(".prcard__feat-item strong")
         val product = Product(url, name, brand, Some(category))
         buffer += product.toCsv + "\n"
       } catch {
--- src/main/scala/org/epicsquad/Product.scala
+++ src/main/scala/org/epicsquad/Product.scala
@@ -1,5 +1,5 @@
 package org.epicsquad
 
-case class Product(url: String, name: String = "", brand: String = "", category: Option[String] = None) {
-  def toCsv = url + ";" + name + ";" + brand + ";" + category.getOrElse("")
+case class Product(url: String, name: String = "", brand: Option[String] = None, category: Option[String] = None) {
+  def toCsv = url + ";" + name + ";" + brand.getOrElse("") + ";" + category.getOrElse("")
 }
