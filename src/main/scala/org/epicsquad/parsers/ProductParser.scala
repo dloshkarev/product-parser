b36fd91b0da2812ffbf937f3fcdb8baee321cb58
Denis Loshkarev <DLoshkarev@energocomm.ru>
2019-03-09 03:25:12 +0300

    added final export

--- src/main/scala/org/epicsquad/parsers/ProductParser.scala
+++ src/main/scala/org/epicsquad/parsers/ProductParser.scala
@@ -2,6 +2,7 @@ package org.epicsquad.parsers
 
 import java.io.FileWriter
 import java.net.URL
+import java.nio.file.{Files, Paths}
 
 import com.typesafe.scalalogging.StrictLogging
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
@@ -13,6 +14,7 @@ import org.epicsquad.{Product, Settings}
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.{DesiredCapabilities, RemoteWebDriver}
 
+import scala.collection.JavaConverters.seqAsJavaListConverter
 import scala.collection.mutable
 import scala.io.Source
 import scala.reflect.io.File
@@ -65,6 +67,43 @@ trait ProductParser extends StrictLogging {
     }
   }
 
+  def exportProducts(productFile: String, outFile: String): Unit = {
+    val products = Source.fromFile(productFile).getLines().toSeq.map(Product.fromCsv)
+    val (correct, incorrect) = products.partition(_.name.nonEmpty)
+    logger.info(s"Found ${incorrect.size} incorrect lines. Try to fix...")
+    val (corrected, stillIncorrect) = restoreProductsRecursively(incorrect)
+
+    val fullCorrect = if (stillIncorrect.isEmpty) correct ++ corrected
+    else correct ++ corrected ++ stillIncorrect
+    Files.write(Paths.get(productFile), fullCorrect.map(_.toCsv).asJava)
+
+    if (stillIncorrect.isEmpty) {
+      logger.info(s"${corrected.size} corrected products added into: $productFile")
+      Files.write(Paths.get(outFile), fullCorrect.map(_.toExport).asJava)
+      logger.info(s"${fullCorrect.size} products exported into: $outFile")
+    } else {
+      logger.error(s"Could not correct all products, keep falling: ${stillIncorrect.size}")
+    }
+  }
+
+  def restoreProductsRecursively(incorrect: Seq[Product], correct: Seq[Product] = Seq(), cnt: Int = 0): (Seq[Product], Seq[Product]) = {
+    if (cnt == 10 || incorrect.isEmpty) (correct, incorrect)
+    else {
+      val (corrected, stillIncorrect) = incorrect
+        .map { p =>
+          logger.info(s"Try: ${p.url}")
+          try {
+            parseProduct(p.url)
+          } catch {
+            case _: Throwable =>
+              logger.error("Failed...:(")
+              p
+          }
+        }.partition(_.name.nonEmpty)
+      restoreProductsRecursively(stillIncorrect, correct ++ corrected, cnt + 1)
+    }
+  }
+
   def parseMeta(doc: JsoupDocument, attrValue: String, attrName: String = "itemprop"): Option[String] = {
     doc >?> extractor("meta[" + attrName + "=" + attrValue + "]", attr("content"))
   }
@@ -112,6 +151,11 @@ trait ProductParser extends StrictLogging {
   }
 
   protected def parseProduct(doc: JsoupDocument, url: String): Product
+
+  protected def parseProduct(url: String): Product = {
+    val doc = browser.get(url)
+    parseProduct(doc, url)
+  }
 }
 
 class ElementExt(e: Element) {
